e3fa9d16068446a9071b6341cf50cf0136fe5204
Eduard Budacu <eduard.budacu@gmail.com>
2021-12-14 12:38:56 +0100

    cleaned the db sample config

--- home/cfg.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('DOC_ROOT', "../");
-require_once(DOC_ROOT."cfg.php");
-?>
--- setari/db.sample.php
+++ setari/db.sample.php
@@ -1,13 +1,6 @@
 <?php
-<<<<<<< HEAD:setari/db.php
 define('DB_HOST', '');
 define('DB_USER', '');
 define('DB_PASS', '');
 define('DB_DATABASE', '');
-=======
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_DATABASE", "gestiune_turabo");
->>>>>>> b757755f5c7329945fe5a28c009c8a954d7c2bb2:setari/db.sample.php
 ?>
